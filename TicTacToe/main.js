8a545e56466f0d6b6156064b90a50439966b9402
Marcus Baptiste <marbap45@gmail.com>
2016-11-10 23:57:52 -0400

    Add base for cell functionality

--- TicTacToe/main.js
+++ TicTacToe/main.js
@@ -1,1 +1,9 @@
-alert("Tic Tac Toe");
+
+var cells = document.querySelectorAll(".cell");
+var i, len = cells.length;
+
+for (i=0; i<len; i++) {
+  cells[i].onclick = function() {
+    alert("Cell " + this.getAttribute("id") + " clicked");
+  };
+}
