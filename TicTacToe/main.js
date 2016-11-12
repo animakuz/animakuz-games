bfbb88b74cc7738afb131e615bc5082d646b0b41
Marcus Baptiste <marbap45@gmail.com>
2016-11-11 23:59:25 -0400

    Update Base cell functionality

--- TicTacToe/main.js
+++ TicTacToe/main.js
@@ -2,8 +2,17 @@
 var cells = document.querySelectorAll(".cell");
 var i, len = cells.length;
 
+var cellVal = [["","",""],["","",""],["","",""]];
+
+
 for (i=0; i<len; i++) {
-  cells[i].onclick = function() {
-    alert("Cell " + this.getAttribute("id") + " clicked");
-  };
+  cells[i].onclick = function() {    
+    var cellInds = [];
+    var cellId = this.getAttribute("id");
+    cellInds[0] = cellId[1];
+    cellInds[1] = cellId[2];
+    
+    cellVal[cellInds[0], cellInds[1]] = "X";
+    alert( cellVal[cellInds[0], cellInds[1]]);
+ };
 }
