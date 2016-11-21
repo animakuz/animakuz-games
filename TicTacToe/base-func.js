7ef2545bcf989424ec7d1238b06769e28a16590d
Marcus Baptiste <marbap45@gmail.com>
2016-11-21 00:14:57 -0400

    Update master branch

--- TicTacToe/base-func.js
+++ TicTacToe/base-func.js
@@ -1,172 +1,181 @@
-//check if variable is function
-//Source - Alex Grande - StackOverflow  - Answer 09/09/11
-function isFunction(objToCheck) {
-  var getType = {};
-  return objToCheck && getType.toString.call(objToCheck) === '[object Function]';
-}
+var baseFunc = {
 
-//get random integer between a specific range
-//Source - Ionuț G. Stan - StackOverflow - Answer 06/10/09
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  isFunction: function(objToCheck) {
+    //check if variable is function
+    //Source - Alex Grande - StackOverflow  - Answer 09/09/11
+    var getType = {};
+    return objToCheck && getType.toString.call(objToCheck) === '[object Function]';
+  },
 
-//truncate decimals to floor
-function floorTrunc(num, numDecimals) {
-  var strNum = String(num);
-  var pointPos = strNum.indexOf('.');
-  strNum = strNum.slice(0, pointPos + numDecimals + 1);
-  return parseFloat(strNum);
-}
+  getRandomInt: function(min, max) {
+    //get random integer between a specific range
+    //Source - Ionuț G. Stan - StackOverflow - Answer 06/10/09
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
 
-//compare two arrays A and B - return true if they are equal in items and length
-function compArrs(arrA, arrB) {
-  var lenA = arrA.length;
-  var lenB = arrB.length;
-  var ind  = lenB;
+  floorTrunc: function(num, numDecimals) {
+    //truncate decimals to floor
+    var strNum = String(num);
+    var pointPos = strNum.indexOf('.');
+    strNum = strNum.slice(0, pointPos + numDecimals + 1);
+    return parseFloat(strNum);
+  },
 
-  if (lenA !== lenB) {
-    return false;
-  } else {      
-    arrA = arrA.sort();
-    arrB = arrB.sort();
+  compArrs: function(arrA, arrB) {
+    //compare two arrays A and B - return true if they are equal in items and length
+    var lenA = arrA.length;
+    var lenB = arrB.length;
+    var ind  = lenB;
 
-    while(ind--) {
-      if (arrA[ind] !== arrB[ind]) {
-        return false;
+    if (lenA !== lenB) {
+      return false;
+    } else {      
+      arrA = arrA.sort();
+      arrB = arrB.sort();
+
+      while(ind--) {
+        if (arrA[ind] !== arrB[ind]) {
+          return false;
+        }
       }
     }
-  }
-    
-  return true;
-}
 
-//check if array A is a sub array of B
-function compSubArr(arrA, arrB) {
-  var str = arrB.join('-');
-  var len = arrA.length;
-  var flag = true;
-
-  while(len--) {
-    var testExp = new RegExp(arrA[len]);
-      
-    if (!testExp.test(str)) {
-      flag = false;
-      break;
-    }
-  }
-
-  return flag;
-}
-
-
-//delete all child nodes from element
-//Source - Gabriel McAdams - StackOverflow - Answer 17/10/10
-function empty(element) {
-  while (element.firstChild) {
-     element.removeChild(element.firstChild);
-  }
-}
-
-//check if an element has a certain class or multiple classes
-function classCheck(element, classesToCheck) {
-  var newClasses = classesToCheck.replace(/\s+/g,'').split(",");
-  var currentClasses = element.className.trim().replace(/\s+/g,' ') + ' ';
-  var ind = newClasses.length;
-  var hasClass = true;
-
-  while(ind--) {
-    var testTerm = new RegExp(newClasses[ind] +' ');
-
-    if (!testTerm.test(currentClasses)) {
-      hasClass = false;
-      break;
-    }  
-  }
+    return true;
+  },  
  
-  return hasClass;
-}
+  compSubArr: function(arrSub, arrCont) {
+    //Check if arrSub is a sub array of arrCont (all the items
+    //in arrSub are contained in arrCont)
+    var lenSub  = arrSub.length;
+    var lenCont = arrCont.length;
+    var indSub;
+    var tempInd;
 
-//add a class or multiple classes to an element
-function addClass(element, classesToAdd) {		
-  var newClasses = classesToAdd.replace(/\s+/g,'').split(",");		
-  var newClassName = element.className.trim().replace(/\s+/g,' ') + ' ';
-  var len = newClasses.length;
-  var ind = 0;
-
-  while(ind < len) { 
-    var testTerm = new RegExp(newClasses[ind] + ' ');
-
-    if (!testTerm.test(newClassName)) {
-      //current className doesn't contain class - add it
-      newClassName += newClasses[ind] + ' ';			
-    }
- 
-    ind++;
-  }
-
-  element.className = newClassName.trim();
-
-}
-
-//remove a class or multiple classes from an element
-function removeClass(element, classesToRemove) { 
-  var classes = classesToRemove.replace(/\s+/g,'').split(",");
-  var ind = classes.length;
-  var newClass = element.className.trim().replace(/\s+/g,' ') + ' ';
-  
-  while(ind--) { 
-    //remove class
-    newClass = newClass.replace(classes[ind] + ' ','');
-  }
-
-  element.className = newClass.trim();
-  
-}
-
-//toggle class or multiple classes on and off 
-function toggleClass(element, classesToToggle) {
-  var classes = classesToToggle.replace(/\s+/g,'').split(",")
-  var newClassName = element.className.trim().replace(/\s+/g,' ') + ' ';
-  var len = classes.length;
-  var ind = 0;
-  
-  while (ind < len) {
-    var testTerm = new RegExp(classes[ind] + ' ');
-    
-    if (testTerm.test(newClassName)) {
-      //class exists - remove it
-      newClassName = newClassName.replace(classes[ind] + ' ', '');
+    if (lenSub > lenCont) {
+      return false;
     } else {
-      //class doesn't exist - add it
-      newClassName += classes[ind] + ' ';
+      indSub = lenSub;
+      
+      while (indSub--) {
+        tempInd = arrCont.indexOf(arrSub[indSub]);
+        if (tempInd == -1) {
+          return false;
+        } else { 
+          arrCont.splice(tempInd, 1);
+        }   
+      }
     }
 
-    ind++;
-  }
+    return true;
+  },
   
-  element.className = newClassName.trim();               
+  empty: function(element) {
+    //delete all child nodes from element
+    //Source - Gabriel McAdams - StackOverflow - Answer 17/10/10
 
-}
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+  },
 
-//modify multiple elements with similar function
-function modifyMultiple(elements, process) {
-  var ind = elements.length;
+  classCheck: function(element, classesToCheck) {
+    //check if an element has a certain class or multiple classes
+    var currentClasses = element.className.trim().replace(/\s+/g,' ') + ' ';
+    var newClasses     = classesToCheck.replace(/\s+/g,'').split(",");
+    var hasClass       = true;
+    var ind            = newClasses.length;
+
+    while(ind--) {
+      var testTerm = new RegExp(newClasses[ind] +' ');
+
+      if (!testTerm.test(currentClasses)) {
+        hasClass = false;
+        break;
+      }  
+    }
+ 
+    return hasClass;
+  },
+
+  addClass: function(element, classesToAdd) {
+    //add a class or multiple classes to an element
+    var newClassName = element.className.trim().replace(/\s+/g,' ') + ' ';
+    var newClasses   = classesToAdd.split(/\s+/);		
+    var len          = newClasses.length;
+    var ind          = 0;
+
+    while(ind < len) { 
+      var testTerm = new RegExp(newClasses[ind] + ' ');
+
+      if (!testTerm.test(newClassName)) {
+        //current className doesn't contain class - add it
+        newClassName += newClasses[ind] + ' ';			
+      }  
+ 
+      ind++;
+    }  
+
+    element.className = newClassName.trim();
+  },
+
+  removeClass: function(element, classesToRemove) { 
+    //remove a class or multiple classes from an element
+    var newClass = element.className.trim().replace(/\s+/g,' ') + ' ';
+    var classes  = classesToRemove.split(/\s+/);
+    var ind      = classes.length;
+     
+    while(ind--) { 
+      //remove class
+      newClass = newClass.replace(classes[ind] + ' ','');
+    }  
+
+    element.className = newClass.trim();
   
-  while(ind--) {
-    process(elements[ind]);
+  },
+
+  toggleClass: function(element, classesToToggle) {
+    //toggle class or multiple classes on and off 
+    var newClassName = element.className.trim().replace(/\s+/g,' ') + ' ';
+    var classes      = classesToToggle.split(/\s+/);
+    var len          = classes.length;
+    var ind          = 0;
+  
+    while (ind < len) {
+      var testTerm = new RegExp(classes[ind] + ' ');
+    
+      if (testTerm.test(newClassName)) {
+        //class exists - remove it
+        newClassName = newClassName.replace(classes[ind] + ' ', '');
+      } else {
+        //class doesn't exist - add it
+        newClassName += ' ' + classes[ind];
+      }
+
+      ind++;
+    }
+  
+    element.className = newClassName.trim();               
+
+  },
+
+  modifyMultiple: function(elements, process) {
+    //modify multiple elements with similar function
+    var ind = elements.length;
+  
+    while(ind--) {
+      process(elements[ind]);
+    }
+  },
+
+  bindMultiple: function(elements, event, handler) {
+    //bind similar event to multiple objects
+    var ind = elements.length;
+
+    while(ind--) {
+      elements[ind][event] = function(event) {
+        handler(this, event);
+      };
+    }
   }
-}
 
-//bind similar event to multiple objects
-function bindMultiple(elements, event, handler) {
-  var ind = elements.length;
-
-  while(ind--) {
-    elements[ind][event] = function(event) {
-      handler(this, event);
-    };
-  }
-}
-
-
+};
--- TicTacToe/main.js
+++ TicTacToe/main.js
@@ -1,65 +1,83 @@
+
+//--Visual Components
+var gameStatus    = document.getElementById("game-status");
+var currentPlayer = document.getElementById("current-player");
+var movesLeft     = document.getElementById("moves-left");
+var newGameBtn    = document.getElementById("new-game");
+var cells         = document.querySelectorAll(".cell");
+
 //--Base Data
-var cells   = document.querySelectorAll(".cell");
-var grid    = [["","",""],["","",""],["","",""]];
-var players = ["X", "O"];
-var game    = {
-  movesLeft: 0,
+var grid     = [["","",""],["","",""],["","",""]];
+var players  = ["X", "O"];
+var game     = {
+  movesLeft: 9,
   currentPlayer: 0  
 };
 
 //--Gameplay
-function nextTurn() {
-  //switch turns
-}
+function resetGame() {
+  var i, j;
 
-function checkGame() {
-  //checks if the board to see whether there is a win
-  //or the game is over (board full) with no win
+  //reset moves
+  game.movesLeft = 9;
+  movesLeft.innerHTML = game.movesLeft;
+  
+  //reset board
+  baseFunc.modifyMultiple(cells, function(cell) { 
+    cell.innerHTML = "";
+  });
+ 
+  //remove new game button
+  baseFunc.addClass(newGameBtn, "hidden");
+  
+  //reset cell values
+  for (i=0; i<3; i++) {
+    for (j=0; j<3; j++) {
+      grid[i][j] = "";
+    }
+  }
 
+  //Set starting player
+  game.currentPlayer = baseFunc.getRandomInt(0,1);
+  currentPlayer.innerHTML = players[game.currentPlayer];  
+  
+  //show game status
+  baseFunc.removeClass(gameStatus, "hidden");
+  
 }
 
 function initGame() {
-  //set board
+  //setup board on initial launching of game
   var i, len = cells.length;
 
   for (i=0; i<len; i++) {
     cells[i].onclick = function() {    
-      var cellId = this.getAttribute("id");
-      var indY  = cellId[1];
-      var indX  = cellId[2];
-      var cell  = this;
+      var cell   = this;
+      var cellId = cell.getAttribute("id");
+      var indY   = cellId[1];
+      var indX   = cellId[2];
 
       if (grid[indY][indX] == "") {
-        grid[indY][indX] = players[0];
-        cell.innerHTML = players[0];
-        addClass(cell, "occupied");   
+        grid[indY][indX] = players[game.currentPlayer];
+        cell.innerHTML = players[game.currentPlayer];
+        baseFunc.addClass(cell, "occupied");  
+     
+        checkGame();        
       } else {
         alert("Cell already occupied!");
       }
     
     };
   }
-
-  //set players
+  
+  //show game status text 
+  baseFunc.removeClass(gameStatus, "hidden");
 
   //launch game
-
+  resetGame();
 }
 
 
-function resetGame() {
-  //reset moves
-  game.movesLeft = 9;
-
-  //reset board
-  //use modify multiple from base func to execute the empty function on all the cells 
-
-  //choose starting player
-  game.currentPlayer = getRandomInt(0,1);
-  
-  initGame();
-
-}
 
 function endGame() {
   //show game over screen and results
@@ -67,6 +85,73 @@ function endGame() {
   //give option to start new game
 }
 
+function nextTurn() {
+  //switch turns
+  if (game.currentPlayer == 0) {
+    game.currentPlayer = 1;
+  } else {
+    game.currentPlayer = 0;
+  }
+  
+  currentPlayer.innerHTML = players[game.currentPlayer];
+}
+
+function checkWin() {
+  //check for different cases that equals a win
+  if (baseFunc.getRandomInt(1,3) == 2) {
+    return players[game.currentPlayer];
+  } else { 
+    return false;
+  }
+}
+
+function newGame() {
+  var resp =  confirm("Would you like to start a new game?");
+        
+  if (resp) {
+    resetGame();
+  } else {
+    alert("Thank you for playing");  
+    baseFunc.addClass(gameStatus, "hidden");
+    baseFunc.removeClass(newGameBtn, "hidden");
+  }
+}
+
+function checkGame() {
+  /*
+    checks the board to see whether there is a win or the 
+    game is over (board full) with no win
+  */
+  var winner;
+  game.movesLeft -= 1;
+  movesLeft.innerHTML = game.movesLeft;
+  
+  winner = checkWin();
+
+  if (winner) {
+    //game over - win
+    alert("Game Over - " + winner + " has won!");
+
+    newGame();
+  } else {   
+    if (game.movesLeft <= 0) {
+      //game over - no win
+
+      alert("Game Over - It's a draw!");
+      newGame();
+     
+    } else {
+      nextTurn();
+    }   
+  }
+}
+
 
 initGame();
 
+
+//Other GUI interaction
+
+newGameBtn.onclick = function() {
+  resetGame();
+};
