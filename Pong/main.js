e9e006572bacb43543dd11cad75523acb8d5a58a
Marcus Baptiste <marbap45@gmail.com>
2016-11-27 22:47:28 -0400

    [Pong] - Add basic data and function structure

--- Pong/main.js
+++ Pong/main.js
@@ -1,13 +1,72 @@
 
-//setup
-var pongC = document.getElementById("pongc");
-var ctx = pongC.getContext("2d");
+//--References to Visual Components
+var canvas = document.getElementById("pong-canvas");
+var ctx    = canvas.getContext("2d");
+var gameLoop; 
+
+//--Base Data and Functions
+var game = {
+  inPlay: false,
+  points: 0,
+  lives : 0,
+  reset : function () {
+    this.inPlay = true;
+    this.points = 0;
+    this.lives  = 3;
+  }
+};
+
+var ball = {
+  size : 50,
+  x    : 0,
+  y    : 0,
+  speed: 0,
+  dx   : 0,
+  dy   : 0,
+  reset: function() {  
+    this.x = 0;
+    this.y = 0;
+    this.speed = 1;
+    this.dx = this.speed;
+    this.dy = this.speed;
+  },
+  move: function() {
+    //Change data that represents position of ball
+    this.x += this.dx;
+    this.y += this.dy;
+
+    //collisions
+  },
+  update: function() {
+    //update visual component of ball
+  }
+};
+
+var paddle = {
+  size: 100,
+  x   : 0,
+  y   : 0,
+  move: function(direction) {
+    //Change data that represents positon of paddle
+    
+  },
+  update: function() {
+    //update visual representation of paddle
+  }  
+}; 
 
 
-//basic functions
-ctx.moveTo(10,10); 
-ctx.lineTo(50, 50);
-ctx.stroke();
+//--Game Update function
+function initGame() {
+  //launch game
+  game.reset() ;
 
-//gameplay
+  gameLoop = setInterval(function() {
+    ball.move();
+    ball.update();
+    paddle.update();
+  }, 32);
+};
 
+initGame();
+  
