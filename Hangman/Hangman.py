33977d4b8e36abd730542d1db2860ec04ee11398
Marcus Baptiste <marbap45@gmail.com>
2018-07-20 11:30:59 -0400

    [Hangman] Made display of guess more readable

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -12,11 +12,11 @@ def get_rand_index(max):
 
 def make_guess_word(word):
     len_word   = len(word)
-    guess_word = ""
+    guess_word = []  
     i          = 0
 
     while i < len_word:
-        guess_word += "_"
+        guess_word.append("_") 
         i += 1
 
     return guess_word
@@ -24,20 +24,37 @@ def make_guess_word(word):
 def check_guess(guess, word, guess_word):
     i              = 0
     len_word       = len(word)
-    new_guess_word = "" 
+    new_guess_word = [] 
     correct_guess  = False
 
     while i < len_word:
         if guess == word[i]:
             #guess matches letter in word
-            new_guess_word += guess
+            new_guess_word.append(guess)
             correct_guess = True
         else: 
-            new_guess_word += guess_word[i]
+            new_guess_word.append(guess_word[i])
         
         i += 1
     
     return [correct_guess, new_guess_word]
+
+def format_guess_word(guess_word):
+    """Result of this algorithm was not desirable
+    result = ""
+    i      = 0
+    w_len  = len(guess_word)    
+    
+    while (i < w_len - 1):
+        if guess_word[i] == "_" or guess_word[i + 1] == "_": 
+            result += guess_word[i] + " "
+        else:
+            result += guess_word[i] 
+        i += 1 
+
+    return result + guess_word[-1] 
+    """
+    return " ".join(guess_word)
 
 def check_win(word, guess_word):
     if word == guess_word:
@@ -77,20 +94,20 @@ def play_game(game_data):
 
         game_data["letters_used"].append([guess])
     
-        if check_win(game_data["word"], game_data["guess_word"]) == True:
-            print "\nYou have won! \nComplete word: " +  game_data["guess_word"]
+        if check_win(game_data["word"], "".join(game_data["guess_word"])) == True:
+            print "\nYou have won! \nComplete word: " +  game_data["word"]
         else:
             print "\nYou have " + str(game_data["tries"]) + " tries left.\n"
             
             if game_data["tries"] <= 0:
                 print "\nYou have lost!\n"
             else:
-                print "\nYour guesses so far: " + game_data["guess_word"] + "\n"
+                print "\nYour guesses so far: " + format_guess_word(game_data["guess_word"]) + "\n"
 
                 play_game(game_data)
 
     else:
-        print "This letter has already been used\n" +  game_data["guess_word"] + "\n"
+        print "This letter has already been used\n" +  format_guess_word(game_data["guess_word"]) + "\n"
 
         play_game(game_data)
  
@@ -100,7 +117,7 @@ print "H A N G M A N\n\n"
 #setup
 words = ['computer', 'program', 'repository', 'recursive', 'processor', 'interface', 'application', 'software', 'keyboard', 'command']
 
-game_data = { "word" : "", "guess_word" : "", "letters_used": [], "tries" : 0 }
+game_data = { "word" : "", "guess_word" : [], "letters_used": [], "tries" : 0 }
 
 ind                     = get_rand_index(10);
 game_data["word"]       = words[ind]
